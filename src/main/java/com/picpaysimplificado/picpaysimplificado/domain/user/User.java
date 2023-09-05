6e6bbafe2ce635a009d19ebe8d5bb7398e58390b
Bruno Gago <bruno.gago@ab-inbev.com>
2023-09-05 08:21:14 -0300

    User entity created

---
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/domain/user/User.java
@@ -0,0 +1,41 @@
+package com.picpaysimplificado.picpaysimplificado.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity(name = "users")
+@Table(name = "TB_USERS")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    @Column(unique = true)
+    private String document; // we are creating a user physical person (CPF) and legal person (CNPJ)
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private BigDecimal balance;
+
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+
+
+}
---
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/domain/user/UserType.java
@@ -0,0 +1,7 @@
+package com.picpaysimplificado.picpaysimplificado.domain.user;
+
+public enum UserType {
+
+    COMMON,
+    MERCHAN
+}
